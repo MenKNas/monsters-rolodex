d298a3baccc30bf5a2a5397096c84725b68e8fbd
Menelaos <men.nasies@gmail.com>
2020-06-24 14:09:19 +0300

    Updates

---
+++ precache-manifest.0f24af6db03b9441487f64d3186d6105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055563f50fee19bf4e975407f282704b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2216594dec0e042a5b43",
+    "url": "/monsters-rolodex/static/css/main.7570b790.chunk.css"
+  },
+  {
+    "revision": "3370f8def62012416adf",
+    "url": "/monsters-rolodex/static/js/2.e96ad927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e96ad927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2216594dec0e042a5b43",
+    "url": "/monsters-rolodex/static/js/main.8151615d.chunk.js"
+  },
+  {
+    "revision": "ac799af20ecf66cd72a4",
+    "url": "/monsters-rolodex/static/js/runtime-main.f14ad4b6.js"
+  }
+]);
